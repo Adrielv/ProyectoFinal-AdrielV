6f6a19109b5c858bb70e3e324a073e9d16899eba
Adriel Villar Vasquez <adriel_villar@ucne.edu.do>
2019-07-14 11:57:59 -0400

    Creando la Entidades

---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public string Nombres { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Usuario = string.Empty;
+            Clave = string.Empty;
+            Nombres = string.Empty;
+            Email = string.Empty;
+            FechaCreacion = DateTime.Now;
+        }
+    }
+}
